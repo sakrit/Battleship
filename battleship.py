e1a4b07c375b91a3fc53ef522ca0bfa7e6317559
Tom Ravenscroft <toravenscroft@gmail.com>
2015-07-21 20:43:25 +0100

    added upper and lower case for the x coordinate

--- battleship.py
+++ battleship.py
@@ -109,7 +109,7 @@ def get_x_coord(x):
     
     d = {'A' : 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6, 'H' : 7, 'I' : 8, 'J' : 9}
     try:
-        return d[x]
+        return d[x.upper()]
     except KeyError as e:
         print 'KeyError:: %s not valid' % (e)
         return -1
